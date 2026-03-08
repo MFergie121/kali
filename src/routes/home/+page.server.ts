8c45908bcc5baf9e573de8370c4ed51fc6ff2525
Max Fergie <maxpfergie@gmail.com>
2026-03-08 13:10:13 +1100

    fix: layout now handles authenticating session

--- src/routes/home/+page.server.ts
+++ src/routes/home/+page.server.ts
@@ -1,12 +1,1 @@
-import type { PageServerLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async (event) => {
-	const session = await event.locals.auth();
-
-	if (!session) {
-		redirect(303, '/auth/login');
-	}
-
-	return { session };
-};
