a0efacefa2315e0a57965d883af3d614a343d91c
Max Fergie <maxpfergie@gmail.com>
2026-03-27 16:17:38 +1100

    fix: if it ain't broke, dont fix it

--- src/lib/db/afl/index.ts
+++ src/lib/db/afl/index.ts
@@ -1,7 +1,19 @@
 import { env } from "$env/dynamic/private";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as schema from "./schema";
 
-const client = postgres(env.DATABASE_URL);
-export const db = drizzle(client, { schema });
+// Cloud SQL URLs have no hostname (e.g. ://user:pass@/db) which breaks
+// Node's URL parser. Insert a dummy host so we can parse it.
+const url = new URL(env.DATABASE_URL.replace("@/", "@localhost/"));
+const socketPath = url.searchParams.get("host");
+
+const client = postgres({
+  user: decodeURIComponent(url.username),
+  pass: decodeURIComponent(url.password),
+  database: url.pathname.replace(/^\//, ""),
+  ...(socketPath
+    ? { host: socketPath }
+    : { host: url.hostname, ...(url.port && { port: Number(url.port) }) }),
+});
+
+export const db = drizzle(client);
